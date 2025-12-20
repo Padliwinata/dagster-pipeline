69719c6a585f51e79d7df1d074a639c84d1b6c92
Padliwinata <rpadliwinata@gmail.com>
2025-12-20 19:14:00 +0700

    fixed wordcloud_maker.py

--- my_dagster_project/__init__.py
+++ my_dagster_project/__init__.py
@@ -1,3 +1,4 @@
 from .news_scraper import news_scraper_repository
+from .wordcloud_maker import wordcloud_repository
 
-__all__ = ["news_scraper_repository"]
+__all__ = ["news_scraper_repository", "wordcloud_repository"]
